c2fd38b555a60c3e77bfe99a56d1c577e2c22eb3
varuncodev <varuncodev@gmail.com>
2026-04-16 02:11:12 +0530

    created card using state updating

---
+++ react4/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Cards from './Components/Cards'
+
+function App() {
+ 
+  return (
+    <>
+    <h1>
+      Hello Varun
+    </h1>
+  <Cards/>
+    </>
+  )
+}
+
+export default App
