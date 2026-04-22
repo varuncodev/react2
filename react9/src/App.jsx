03e5a9f7d8157122e37151184ff25ec4b0560b9f
varuncodev <varuncodev@gmail.com>
2026-04-23 01:44:33 +0530

    all problem fixed

--- react9/src/App.jsx
+++ react9/src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Component/Button'
 import Color from './Component/Color'
 
 const App = () => {
+  const [colo ,setColo] =useState('');
+
+
+const colors= ['red','orange','pink','blue','yellow','green','black','violet']
+
+
   return (
-    <div>
-      <Color/>
+   <>
+    <h1 className='justify-center flex mt-2 '>Color Switcher</h1>
+
+
+    <div className='flex gap-4 my-7 justify-center   '>
+    {colors.map((items)=>{
+      return(
+        <Button key={items} color={items} setColo={setColo}>
+        {items}
+        </Button> 
+      )
+    })
+    }
     </div>
+    <Color color={colo}/>
+   </>
   )
 }
 
--- react9/src/Component/Color.jsx
+++ react9/src/Component/Color.jsx
@@ -1,32 +1,12 @@
 import React from 'react'
 
-const Color = () => {
-
-    function Colo(){
-        
-    }
-
-
+const Color = ({color}) => {
   return (
-      <>
-    <div className='font-bold text-3xl' onClick={Colo}>
-      <h1>heelo world</h1>
-      <div className='justify-center flex gap-4 '>
-      <button className='text-red-600 bg-red-200 rounded-lg px-2'>Red</button>
-      <button className='text-blue-600 bg-red-200 rounded-lg px-2'>Blue</button>
-      <button className='text-orange-600 bg-red-200 rounded-lg px-2'>Orange</button>
-      <button className='text-pink-600 bg-red-200 rounded-lg px-2'>Pink</button>
-      <button className='text-yellow-600 bg-red-200 rounded-lg px-2'>Yellow</button>
-      <button className='text-violet-600 bg-red-200 rounded-lg px-2'>Violet</button>
-      <button className='text-green-600 bg-red-200 rounded-lg px-2'>Green</button>
-      </div>
+    <div style={{
+        backgroundColor:color,
+    }} className='h-[30rem] w-[30rem] rounded-lg m-auto border-2'>
       
     </div>
-    <div className='h-[20rem] w-[20rem] bg-red-100 m-auto mt-30'>{Colo}
-
-
-      </div>
-    </>
   )
 }
 
